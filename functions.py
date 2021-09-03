7cd8dbabc0e1f436a6e218a3f20d24f79f627321
1-mauricio <mauriiciioallves@hotmail.com>
2021-09-03 03:30:37 -0300

    first menu

--- functions.py
+++ functions.py
@@ -1,7 +1,36 @@
 import pandas as pd
 import mysql.connector
 
+# MENUS
+def inicio():
+    print("------------------------------------------------")
+    print("OLÁ, ESTE É UM PROGRAMA FEITO PARA O USO DE DADOS")
+    print("RELACIONADOS AS OLIMPIEDAS E PARA A MANIPULAÇÃO")
+    print("DO MYSQL")
+    print("------------------------------------------------")
 
+def menu_inicial():
+    print("\n----------------------------------------------------")
+    print("------------------------MENU------------------------")
+    print("1 - Se conectar ao banco de dados (Se conecte a um database para conseguir manipular as tabelas)")
+    print("2 - Criar novo banco de dados")
+    print("3 - Excluir algum banco de dados")
+    print("4 - Executar algum comando no MySQL")
+    print("5 - Mostrar os bancos de dados existentes")
+    print("6 - Mostrar esse menu novamente")
+    print("----------------------------------------------------")
+    print("----------------------------------------------------")
+
+def menu_tabela():
+    print("\n----------------------------------------------------")
+    print("------------------------MENU------------------------")
+    print("1 - Mostrar as tabelas existentes")
+    print("2 - Manipular dados das olimpiedas")
+    print("3 - Voltar ao menu anterior")
+    print("----------------------------------------------------")
+    print("----------------------------------------------------")
+
+# CONEXAO
 def conexao_database(database):
     cnx = mysql.connector.connect(
         host='localhost', 
@@ -17,6 +46,7 @@ def conexao_inicial():
     
     return cnx
 
+# MENU INICIAL
 def create_database(database, cnx, cursor):
     create_database = (f"CREATE DATABASE {database}")
 
@@ -29,6 +59,16 @@ def drop_database(database, cnx, cursor):
     cursor.execute(drop_database)
     cnx.commit()
 
+def show_databases(cnx, cursor):
+    cursor.execute("SHOW DATABASES")
+    cont = 1
+    print("\n---------------------------------------")
+    for database in cursor:
+        print(f"{cont} - {database[0]}")
+        cont += 1
+    print("---------------------------------------\n")
+
+# MENU TABELAS
 def create_medals(cnx, cursor):
     create_medals = ("CREATE TABLE medals ( rank INT(10), team VARCHAR(100), gold INT(4), silver INT(4), bronze INT(4), total INT(4), rank_total INT(10))")
 
@@ -42,7 +82,7 @@ def delete_table(table, where, cnx, cursor):
     cnx.commit()
 
 def insert_medals(cnx, cursor):
-    rank_medalhas = pd.read_excel("Olimpiedas dados\Medals.xlsx")
+    rank_medalhas = pd.read_excel("data\Medals.xlsx")
     for i in range(0, len(rank_medalhas)):
         rank = rank_medalhas["Rank"][i]
         team = rank_medalhas["Team/NOC"][i]
@@ -69,8 +109,8 @@ def command(command, cnx, cursor):
     cursor.execute(com)
     cnx.commit()
 
-def show_databases(cnx, cursor):
-    cursor.execute("SHOW DATABASES")
+def show_tables(cursor):
+    cursor.execute("SHOW TABLES")
     cont = 1
     print("\n---------------------------------------")
     for database in cursor:
@@ -78,44 +118,3 @@ def show_databases(cnx, cursor):
         cont += 1
     
     print("---------------------------------------\n")
-
-def menu_inicial():
-    print("\n----------------------------------------------------")
-    print("------------------------MENU------------------------")
-    print("1 - Se conectar ao banco de dados (Se conecte a um database para conseguir manipular as tabelas)")
-    print("2 - Criar novo banco de dados")
-    print("3 - Excluir algum banco de dados")
-    print("4 - Executar algum comando no MySQL")
-    print("5 - Mostrar os bancos de dados existentes")
-    print("6 - Mostrar esse menu novamente")
-    print("----------------------------------------------------")
-    print("----------------------------------------------------")
-
-def menu_tabela():
-    print("\n----------------------------------------------------")
-    print("------------------------MENU------------------------")
-    print("1 - Se conectar ao banco de dados (Se conecte a um database para conseguir manipular as tabelas)")
-    print("2 - Criar novo banco de dados")
-    print("3 - Excluir algum banco de dados")
-    print("4 - Executar algum comando no MySQL")
-    print("5 - Mostrar as tabelas existentes")
-    print("----------------------------------------------------")
-    print("----------------------------------------------------")
-
-def show_tables(cnx, cursor):
-    cursor.execute("SHOW DATABASES")
-    cont = 1
-    print("\n---------------------------------------")
-    for database in cursor:
-        print(f"{cont} - {database[0]}")
-        cont += 1
-    
-    print("---------------------------------------\n")
-    
-
-def inicio():
-    print("------------------------------------------------")
-    print("OLÁ, ESTE É UM PROGRAMA FEITO PARA O USO DE DADOS")
-    print("RELACIONADOS AS OLIMPIEDAS E PARA A MANIPULAÇÃO")
-    print("DO MYSQL")
-    print("------------------------------------------------")
--- main.py
+++ main.py
@@ -4,67 +4,107 @@ import functions
 from mysql.connector import errorcode
 
 aux = 0
+aux_all = 0
 end_all = False
 menu_inicio = True
-menu_tabela = True
+menu_database = True
+menu_olympics = True
 
 functions.inicio()
 while not end_all:
-	while menu_inicio:
-		try:
-			if aux == 0:
-				functions.menu_inicial()
-				aux += 1
+	if aux_all == 0:
+		while menu_inicio:
+			try:
+				if aux == 0:
+					functions.menu_inicial()
+					aux += 1
 
-			menu_inicial = input(": ")
+				menu_inicial = input(": ")
 
-			if menu_inicial == "1":
-				database = input("Digite o nome do banco de dados para tentar a conexão: ")
-				cnx = functions.conexao_database(database)
-				cursor = cnx.cursor()
+				if menu_inicial == "1":
+					database = input("Digite o nome do banco de dados para tentar a conexão: ")
+					cnx = functions.conexao_database(database)
+					cursor = cnx.cursor()
 
-				print("Conexão feita com sucesso")
-
-				menu_next = input("Você gostaria de acessar esse banco de dados?(S/N): ")
-				if menu_next == "S":
+					print("Conexão feita com sucesso")
+					aux_all += 1
 					menu_inicio = False
+					menu_database = True
 
-			elif menu_inicial == "2":
-				cnx_i = functions.conexao_inicial()
+				elif menu_inicial == "2":
+					cnx_i = functions.conexao_inicial()
 
-				cursor_i = cnx_i.cursor()
-				nome = input("Digite o nome do banco de dados: ")
-				functions.create_database(nome, cnx_i, cursor_i)
-				print("Banco de dados criado com sucesso.\n")
+					cursor_i = cnx_i.cursor()
+					nome = input("Digite o nome do banco de dados: ")
+					functions.create_database(nome, cnx_i, cursor_i)
+					print("Banco de dados criado com sucesso.\n")
 
-			elif menu_inicial == "3":
-				drop_database = input("Digite o noem do banco de dados que deseja exluir: ")
+				elif menu_inicial == "3":
+					drop_database = input("Digite o nome do banco de dados que deseja exluir: ")
 
-				cnx_i = functions.conexao_inicial()
-				cursor_i = cnx_i.cursor()
+					cnx_i = functions.conexao_inicial()
+					cursor_i = cnx_i.cursor()
 
-				functions.drop_database(drop_database, cnx_i, cursor_i)
+					functions.drop_database(drop_database, cnx_i, cursor_i)
 
-				print(f"Banco de dados {drop_database} excluido com sucesso")
+					print(f"Banco de dados {drop_database} excluido com sucesso\n")
+					
+				elif menu_inicial == "4":
+					comando = input("Digite o comando que deseja: ")
+
+					cnx_i = functions.conexao_inicial()
+					cursor_i = cnx_i.cursor()
+
+					functions.command(comando, cnx_i, cursor_i)
+					print("Comando executado com sucesso.\n")
+
+				elif menu_inicial == "5":
+					cnx_i = functions.conexao_inicial()
+					cursor_i = cnx_i.cursor()
+
+					functions.show_databases(cnx_i, cursor_i)
 				
-			elif menu_inicial == "4":
-				comando = input("Digite o comando que deseja: ")
+				elif menu_inicial == "6":
+					aux = 0
+				
+			except mysql.connector.errors.DatabaseError:
+				print("Não existe nenhum banco de dados com esse nome. Você pode criar um ou tentar outro nome.")
 
-				cnx_i = functions.conexao_inicial()
-				cursor_i = cnx_i.cursor()
+	elif aux_all == 1:
+		while menu_database:
+			functions.menu_tabela()
+			menu_data = int(input(": "))
 
-				functions.command(comando, cnx_i, cursor_i)
-
-			elif menu_inicial == "5":
-				cnx_i = functions.conexao_inicial()
-				cursor_i = cnx_i.cursor()
-
-				functions.show_databases(cnx_i, cursor_i)
+			if menu_data == 1:
+				functions.show_tables(cursor)
 			
-			elif menu_inicial == "6":
+			elif menu_data == 2:
+				aux_all += 1
+				menu_database = False
+				menu_olympics = True
+			
+			elif menu_data == 3:
+				aux_all -= 1
 				aux = 0
-			
-		except mysql.connector.errors.DatabaseError:
-			print("Não existe nenhum banco de dados com esse nome. Você pode criar um ou tentar outro nome.")
+				menu_database = False
+				menu_inicio = True
+				
+	elif aux_all == 2:
+		while menu_olympics:
+			menu_oly = int(input(": "))
+			if menu_oly == 1:
+				functions.create_medals(cnx, cursor)
 
-	
+			if menu_oly == 2:
+				functions.delete_table("medals", 'gold < 1000', cnx, cursor)
+
+			if menu_oly == 3:
+				functions.insert_medals(cnx, cursor)
+
+			if menu_oly == 4:
+				functions.drop_table("medals", cnx, cursor)
+			
+			elif menu_oly == 5:
+				aux_all -= 1
+				menu_olympics = False
+				menu_database = True
